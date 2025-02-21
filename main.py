9d8620792e2d8ccb8a83f59da1637c1b2f7d1017
KANNAN PERUMAL <123189658+pkannan2002@users.noreply.github.com>
2025-02-21 13:31:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,4 +34,5 @@ async def ping_self():
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
