6849b6a361175f8884862eb393aa658458e65343
KANNAN PERUMAL <123189658+pkannan2002@users.noreply.github.com>
2025-02-21 11:57:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ async def ping_self():
     import requests
     requests.get("https://fastapi-ai-test.onrender.com/")
 
+import uvicorn
+
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
